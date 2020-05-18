b0ea96d014f98d7347c12362ca906b4359acee60
Brendan Chabannes <brendan.chabannes@gmail.com>
2020-05-18 23:54:33 +0200

    numéros de version plus courts

--- build.ps1
+++ build.ps1
@@ -2,6 +2,6 @@
 # Pas de maintenance prévue du script
 
 $now = Get-Date -UFormat "%Y-%m-%d_%T"
-$sha1 = (git rev-parse HEAD).Trim()
+$sha1 = (git rev-parse --short HEAD).Trim()
 
 go build -ldflags "-X main.sha1ver=$sha1 -X main.buildTime=$now"

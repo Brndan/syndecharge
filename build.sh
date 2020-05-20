62c54a0d1e4ea2e607aa554946a75eeaf3549db3
Brendan Chabannes <brendan.chabannes@gmail.com>
2020-05-20 20:26:46 +0200

    Modification du script de build

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 
 
 now=$(date +'%Y-%m-%d_%T')
-LDFLAGS=(-ldflags="-s -w")
 
 if [ "$1" = "install" ]
 then
@@ -11,5 +10,5 @@ exit 0
 
 fi
 
-env GOOS=linux GOARCH=amd64 go build -ldflags "-X main.sha1ver=`git rev-parse --short HEAD` -X main.buildTime=$now" "${LDFLAGS}" -o dist/ github.com/Brndan/syndecharge
-env GOOS=windows GOARCH=amd64 go build -ldflags "-X main.sha1ver=`git rev-parse --short HEAD` -X main.buildTime=$now" "${LDFLAGS}" -o dist/ github.com/Brndan/syndecharge
+env GOOS=linux GOARCH=amd64 go build -ldflags "-X main.sha1ver=`git rev-parse --short HEAD` -X main.buildTime=$now"  -o dist/ github.com/Brndan/syndecharge
+env GOOS=windows GOARCH=amd64 go build -ldflags "-X main.sha1ver=`git rev-parse --short HEAD` -X main.buildTime=$now" -o dist/ github.com/Brndan/syndecharge

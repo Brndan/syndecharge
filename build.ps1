79034c55653951e6ef1f8a886cfa62d02a31a53d
Brendan Chabannes <brendan.chabannes@gmail.com>
2020-05-21 17:29:28 +0200

    Optimiser la compilation

--- build.ps1
+++ build.ps1
@@ -4,4 +4,4 @@
 $now = Get-Date -UFormat "%Y-%m-%d_%T"
 $sha1 = (git rev-parse --short HEAD).Trim()
 
-go build -ldflags "-X main.sha1ver=$sha1 -X main.buildTime=$now"
+go build -ldflags "-X main.sha1ver=$sha1 -X main.buildTime=$now -w -s"
